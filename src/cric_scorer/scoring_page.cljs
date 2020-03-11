afe008da55e517a916a51adca4e0ef004cea4285
Tilak Puli <tilakpuli15@gmail.com>
2020-03-11 17:42:58 +0530

    update score header to have stat

--- src/cric_scorer/scoring_page.cljs
+++ src/cric_scorer/scoring_page.cljs
@@ -6,12 +6,19 @@
    [:div.score-info [:span.runs-info [:span.runs (str (:runs score-info) " / " (:wickets score-info))]
           [:span (str "( " (:over score-info) " / " total-overs " )")]]]])
 
+(defn score-stat [stat]
+  [:div.score-stat
+   [:span.stat (str "CRR : " (:CRR stat))]
+   [:span.stat (str "RRR : " (:RRR stat))]])
+
 (defn score-header [score-infos]
   [:div.score-comp
    (score-display (:team1 score-infos) (:total-overs score-infos))
-   (score-display (:team2 score-infos) (:total-overs score-infos))])
+   (score-display (:team2 score-infos) (:total-overs score-infos))
+   (score-stat (:stat score-infos))])
 
 (defn scoring-page [match]
   [:div.scoring-page (score-header {:team1       {:name "Tilak's" :over 4.4 :runs 10 :wickets 2}
                                     :team2       {:name "Dheeraj's" :over 0.0 :runs 0 :wickets 0}
+                                    :stat        {:CRR 2.2 :RRR 3}
                                     :total-overs 12})])
